5be273704c6d1554151650a5ad498004967c51b0
Gabriellocp <gabriel_campista@hotmail.com>
2022-04-16 19:24:52 -0300

    feat: add field `tipos` on infos

--- lib/data/infos.dart
+++ lib/data/infos.dart
@@ -4,17 +4,20 @@ List<dynamic> infos = [
     'valorMinimoMensal': 1000,
     'valorMaximoMensal': 40000,
     'desconto': .2,
+    'tipos': ['Pessoa Física', 'Pessoa Jurídica']
   },
   {
     'nome': 'EnerLimpa',
     'valorMinimoMensal': 10000,
     'valorMaximoMensal': 80000,
     'desconto': .25,
+    'tipos': ['Pessoa Física']
   },
   {
     'nome': 'EnerGrande',
     'valorMinimoMensal': 40000,
     'valorMaximoMensal': 100000,
     'desconto': .3,
+    'tipos': ['Pessoa Jurídica']
   }
 ];
