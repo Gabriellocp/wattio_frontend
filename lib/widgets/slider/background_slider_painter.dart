c3afab4dcd346c34f68e2311445bef0ea90629d9
Gabriellocp <gabriel_campista@hotmail.com>
2022-04-16 13:09:42 -0300

    update: update BackgroundSliderPainter to show progress

--- lib/widgets/slider/background_slider_painter.dart
+++ lib/widgets/slider/background_slider_painter.dart
@@ -1,46 +1,54 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:wattio_frontend/context/global_context.dart';
-import 'package:wattio_frontend/helpers/screen_helper.dart';
+import 'package:wattio_frontend/helpers/math_helper.dart';
 import 'package:wattio_frontend/styles/colors.dart';
 
 class BackgroundSliderPainter extends CustomPainter {
-  const BackgroundSliderPainter();
+  BackgroundSliderPainter({
+    required this.center,
+    required this.backgroundRadius,
+    required this.sweep,
+    required this.start,
+    required this.text,
+  });
+  final Offset center;
+  double backgroundRadius;
+  final double sweep;
+  final double start;
+  final String text;
   @override
   void paint(Canvas canvas, Size size) {
-    double heightCorrector, backgroundRadius, mainLayerRadius;
+    double strokeWidth = 30.0;
+    double startAngle = start;
     // double radius1, radius2, diff;
     // Back circle (blue)
     Paint paint = Paint();
     paint.color = AppColors.bluePrimary;
-    paint.strokeWidth = 5;
-    paint.strokeCap = StrokeCap.round;
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = strokeWidth;
 
-    backgroundRadius = size.height * .25;
-    heightCorrector = 100;
-    canvas.drawCircle(
-        Offset(
-          size.width / 2,
-          size.height / 2 - backgroundRadius + heightCorrector,
-        ),
-        backgroundRadius,
-        paint);
+    Paint handler = Paint();
+    handler.color = AppColors.yellowPrimary;
+    handler.style = PaintingStyle.stroke;
+    handler.strokeCap = StrokeCap.round;
+    handler.strokeWidth = strokeWidth;
+    canvas.drawCircle(center, backgroundRadius, paint);
     paint.color = Colors.white;
+    // Draw handler
 
-    mainLayerRadius = size.height * .24;
-    canvas.drawCircle(
-        Offset(
-          size.width / 2,
-          size.height / 2 -
-              mainLayerRadius -
-              (backgroundRadius - mainLayerRadius) +
-              heightCorrector,
-        ),
-        mainLayerRadius,
-        paint); // Text inside the circle
-    TextSpan span = const TextSpan(
-      text: 'Teste',
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: backgroundRadius),
+      startAngle,
+      sweep,
+      false,
+      handler,
+    );
+
+    // Text inside the circle
+    TextSpan span = TextSpan(
+      text: text,
       style: TextStyle(
         fontSize: 40.0,
         fontWeight: FontWeight.bold,
@@ -57,16 +65,14 @@ class BackgroundSliderPainter extends CustomPainter {
     // Top-left is the origin (0,0), so we need to get it to always
     // Draw the text fixed on its center
     Offset centerText = Offset(
-      (size.width - textPainter.width) / 2,
-      (size.height - textPainter.height) / 2 -
-          backgroundRadius +
-          heightCorrector,
+      (center.dx - textPainter.width / 2),
+      (center.dy - textPainter.height / 2),
     );
     textPainter.paint(canvas, centerText);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
